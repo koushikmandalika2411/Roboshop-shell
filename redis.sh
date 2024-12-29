9130c11cf466f45101d63ecdb7a40eba630efbcd
Koushik Sai <mandalikakoushik@gmail.com>
2024-12-30 00:14:52 +0530

    updated frontend-dev

--- redis.sh
+++ redis.sh
@@ -1,6 +1,6 @@
 dnf module disable redis -y
 dnf module enable redis -y
-dnf install rebis -y
+dnf install redis -y
 
 sed -i -e 's|127.0.0.1|0.0.0.0|' -e '/procented-mode/ c protected-mode no' /etc/redis/redis.conf
 
