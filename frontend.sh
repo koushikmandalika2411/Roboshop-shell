cc8d2d13145bb37f51899cee9c8aa1668044a509
Koushik Sai <mandalikakoushik@gmail.com>
2024-12-29 19:14:55 +0530

    added frontend-dev and nginx.conf files

---
+++ frontend.sh
@@ -0,0 +1,18 @@
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+
+cp nginx.conf /etc/nginx/nginx.conf
+
+systemctl enable nginx
+systemctl start nginx
+
+rm -rf /usr/share/nginx/html/*
+
+rm -f /temp/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+
+systemctl restart nginx
