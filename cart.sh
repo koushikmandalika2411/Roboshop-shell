9398882cefb03ae081d4fc27d358d761757dc0d6
Koushik Sai <mandalikakoushik@gmail.com>
2025-01-04 00:05:22 +0530

    Declared a Function NODEJS when required

--- cart.sh
+++ cart.sh
@@ -4,16 +4,7 @@ app_name=cart
 cp $app_name.service /etc/systemd/system/$app_name.service
 
 NODEJS
-useradd roboshop
 
-rm -rf /app
-mkdir /app
-
-curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
-cd /app
-unzip /tmp/$app_name.zip
-cd /app
-npm install
 systemctl daemon-reload
 systemctl enable $app_name
 systemctl restart $app_name
--- user.sh
+++ user.sh
@@ -4,18 +4,6 @@ cp $app_name.service /etc/systemd/system/$app_name.service
 
 NODEJS
 
-$app_nameadd roboshop
-
-rm -rf /app
-mkdir /app
-
-curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
-cd /app
-unzip /tmp/$app_name.zip
-
-cd /app
-npm install
-
 systemctl daemon-reload
 systemctl enable $app_name
 systemctl restart $app_name
