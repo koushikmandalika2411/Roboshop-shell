ec47382d572342800ac05a24e499532e505a5fc0
Koushik Sai <mandalikakoushik@gmail.com>
2025-01-04 00:21:42 +0530

    Declared a function inside a Function

--- common.sh
+++ common.sh
@@ -1,5 +1,12 @@
 dir=$(pwd)
 
+SYSTEMD_SETUP() {
+  cp $dir/$app_name.service /etc/systemd/system/$app_name.service
+
+    systemctl daemon-reload
+    systemctl enable $app_name
+    systemctl restart $app_name
+}
 NODEJS() {
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
@@ -17,8 +24,24 @@ unzip /tmp/$app_name.zip
 cd /app
 npm install
 
-cp $dir/$app_name.service /etc/systemd/system/$app_name.service
-systemctl daemon-reload
-systemctl enable $app_name
-systemctl restart $app_name
+SYSTEMD_SETUP
+}
+
+JAVA() {
+  dnf install maven -y
+
+  useradd roboshop
+
+  rm -rf /app
+  mkdir /app
+  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
+  cd /app
+  unzip /tmp/$app_name.zip
+
+  cd /app
+  mvn clean package
+  mv target/$app_name-1.0.jar $app_name.jar
+
+  SYSTEMD_SETUP
+
 }
--- shipping.sh
+++ shipping.sh
@@ -1,23 +1,7 @@
+source ./common.sh
 app_name=shipping
-cp $app_name.service /etc/systemd/system/$app_name.service
-dnf install maven -y
 
-useradd roboshop
-
-rm -rf /app
-mkdir /app
-curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
-cd /app
-unzip /tmp/$app_name.zip
-
-cd /app
-mvn clean package
-mv target/$app_name-1.0.jar $app_name.jar
-
-systemctl daemon-reload
-
-systemctl enable $app_name
-systemctl restart $app_name
+JAVA
 
 dnf install mysql -y
 
