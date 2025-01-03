90223c37c7de29ac09d6cfd9100ec8493a858042
Koushik Sai <mandalikakoushik@gmail.com>
2025-01-04 00:39:48 +0530

    Declared Log file in common for not getting lenghty logs while running commands

--- catalogue.sh
+++ catalogue.sh
@@ -1,11 +1,8 @@
 source ./common.sh
 app_name=catalogue
 
-cp $app_name.service /etc/systemd/system/$app_name.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-
 NODEJS
 
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y
-
 mongosh --host mongodb-dev.azdevops.shop </app/db/master-data.js
--- common.sh
+++ common.sh
@@ -1,50 +1,52 @@
 dir=$(pwd)
+log_file=/tmp/roboshop.log
 
 SYSTEMD_SETUP() {
   cp $dir/$app_name.service /etc/systemd/system/$app_name.service
 
-    systemctl daemon-reload
-    systemctl enable $app_name
-    systemctl restart $app_name
+    systemctl daemon-reload >$log_file
+    systemctl enable $app_name >$log_file
+    systemctl restart $app_name >$log_file
 }
 APP_PREREQ(){
    useradd roboshop
 
     rm -rf /app
-    mkdir /app
-    curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
-    cd /app
-    unzip /tmp/$app_name.zip
+    mkdir /app >$log_file
+    curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip >$log_file
+    cd /app >$log_file
+    unzip /tmp/$app_name.zip >$log_file
 
-    cd /app
+    cd /app >$log_file
 }
 NODEJS() {
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+echo Disable Default  NodeJS Version
+dnf module disable nodejs -y >$log_file
+dnf module enable nodejs:20 -y >$log_file
+dnf install nodejs -y >$log_file
 
 APP_PREREQ
-npm install
+npm install >$log_file
 
 SYSTEMD_SETUP
 }
 
 JAVA() {
-  dnf install maven -y
+  dnf install maven -y >$log_file
 
   APP_PREREQ
 
-  mvn clean package
-  mv target/$app_name-1.0.jar $app_name.jar
+  mvn clean package >$log_file
+  mv target/$app_name-1.0.jar $app_name.jar >$log_file
 
   SYSTEMD_SETUP
 }
 
 PYTHON(){
-  dnf install python3 gcc python3-devel -y
+  dnf install python3 gcc python3-devel -y >$log_file
 
   APP_PREREQ
-  pip3 install -r requirements.txt
+  pip3 install -r requirements.txt >$log_file
 
  SYSTEMD_SETUP
 }
